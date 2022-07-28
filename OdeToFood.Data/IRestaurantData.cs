e57ef9118d62533c34de9bc5e06dbd0de4f93f1e
cassi <cassi@DESKTOP-RCF6VQ7>
2022-07-27 23:06:59 -0300

    Adicionar arquivos de projeto.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Kireina", Location="Vila Regina", Cuisine=CuisineType.Japanese},
+                new Restaurant {Id = 2, Name = "Juliete", Location = "Conjunto Primavera", Cuisine=CuisineType.Pizzeria},
+                new Restaurant {Id = 3, Name = "Cleiton Burger", Location = "Residencial Triunfo", Cuisine=CuisineType.Hamburger}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                    select r;
+        }
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        public string Message { get; set; }
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+        public void OnGet()
+        {
+            Message = this.config["Message"];
+        }
+    }
+}
